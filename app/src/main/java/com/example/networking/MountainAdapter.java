3eace142d4cbe1a56912507f9989ef79d4a315fa
Albin Dahlén <b21albda@student.his.se>
2022-04-26 14:24:58 +0200

    Created MountainAdapter and mountainItem

---
+++ app/src/main/java/com/example/networking/MountainAdapter.java
@@ -0,0 +1,33 @@
+package com.example.networking;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MountainAdapter extends RecyclerView.Adapter<MountainAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
